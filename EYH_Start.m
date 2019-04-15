14ce5d1134fcf28433fc92459b680e695ad1ef1f
lirangazit <47790066+lirangazit@users.noreply.github.com>
2019-04-15 14:47:43 -0400

    Add files via upload
    Use this function to start the EYH session

---
+++ EYH_Start.m
@@ -0,0 +1,31 @@
+function [] = EYH_Start(RobotName)
+% function [] = EYH_Start(RobotName)
+% Run this function to start a new EYH session.
+%
+% RobotName = A string with the name of the Create you want to connect to
+%
+% This function will call the Init functions from the Create Matlab toolbox
+% then wait for a prompt from the user to make sure the connection has been
+% properly established, and run the main Maze program.
+
+
+% Call CreatePiInit function to initialize connection with the robot 
+Ports = CreatePiInit(RobotName);
+CreatePort = Ports.create;
+
+% Make sure all the init stuff is done
+prompt = 'Look at the terminal command window. Does it say "Ready for Command!"? y/n [y]';
+str = input(prompt,'s');
+if isempty(str)
+    str = 'y';
+end
+
+% Run the maze program if all good.
+if (strcmp(str,'y') || strcmp(str,'Y'))
+    EYH_Maze_Program(CreatePort);
+else 
+    CreatePiShutdown(Ports);
+    return;
+end
+    
+
